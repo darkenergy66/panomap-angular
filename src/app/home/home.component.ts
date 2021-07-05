75a24dc1accdec6c03912673a38851d23045cb75
darkenergy66 <iancollis@evermore.co.uk>
2021-07-05 23:09:23 +0100

    WiP data service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageDataService } from '../image-data.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private imageData: ImageDataService) { }
 
   ngOnInit(): void {
   }
--- src/app/image-data.service.ts
+++ src/app/image-data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImageDataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.http.get(environment.imagesBaseUrl + 'maps-list.json').toPromise().then( mapsList => {
+      console.log('mapsList', mapsList);
+
+    })
+  }
 }
